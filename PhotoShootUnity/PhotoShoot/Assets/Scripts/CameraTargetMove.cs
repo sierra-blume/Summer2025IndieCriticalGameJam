d4c04edb2fdb4157bf45320a52ebd81f10ef0cf3
Alexander.Cowell <alexander.cowell@mymail.champlain.edu>
2025-08-13 17:28:02 -0400

    added clamps to movement to constrain camera

--- PhotoShootUnity/PhotoShoot/Assets/Scripts/CameraTargetMove.cs
+++ PhotoShootUnity/PhotoShoot/Assets/Scripts/CameraTargetMove.cs
@@ -4,18 +4,24 @@ using UnityEngine.InputSystem;
 public class CameraTargetMove : MonoBehaviour
 {
     [SerializeField] int sensitivity = 100;
+    //Rigidbody2D rb;
+
+    public Vector2 xClamp;
+    public Vector2 yClamp;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 cameraMov = transform.position + new Vector3(Input.GetAxis("Mouse X")*Time.deltaTime*sensitivity, Input.GetAxis("Mouse Y")*Time.deltaTime*sensitivity);
+        cameraMov.x = Mathf.Clamp(cameraMov.x, xClamp.x, xClamp.y);
+        cameraMov.y = Mathf.Clamp(cameraMov.y, yClamp.x, yClamp.y);
         transform.position = cameraMov;
     }
 }
