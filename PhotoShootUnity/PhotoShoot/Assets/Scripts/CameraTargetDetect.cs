3d1f5aa2e53f149b27a9a84930ccb38880daf0ea
NickMancuso2026 <nicholas.mancuso@mymail.champlain.edu>
2025-08-14 10:36:55 -0400

    Dynamic Text UI!
    pulls the names of each target in the "currentTargets" list, turns them into a string, and displays them. Whenever a target is clicked, that item goes away in the list. It only updates the tmp text box when a change is detected

--- PhotoShootUnity/PhotoShoot/Assets/Scripts/CameraTargetDetect.cs
+++ PhotoShootUnity/PhotoShoot/Assets/Scripts/CameraTargetDetect.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
@@ -62,7 +63,10 @@ public class CameraTargetDetect : MonoBehaviour
     }
     void Awake()
     {
-        currentTargets = new List<GameObject>();
+        if (currentTargets == null)
+        {
+            currentTargets = new List<GameObject>();
+        }
         Cursor.lockState = CursorLockMode.Locked;
     }
 }
